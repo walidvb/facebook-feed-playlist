f7c58592e57ef921deb535e74b08c0b9f115c4d6
Walid <hello@walidvb.com>
2021-05-07 17:54:26 -0300

    fetch posts!

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,32 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../components/layout'
+import * as Facebook from 'fb-sdk-wrapper';
+import { useSession } from 'next-auth/client';
 
 const useFeed = () => {
   const [posts, setPosts] = useState([])
+  const session = useSession()
+  useEffect(() => {
+    (async () => {
+      await Facebook.load()
+      await Facebook.init({
+        appId: '926693044836109',
+      });
+      const { authResponse: { accessToken } } = await Facebook.getLoginStatus()
+      console.log(accessToken)
+      console.log('asda')
+      Facebook.login({
+        scope: 'public_profile,email',
+        return_scopes: true
+      }).then(() => {
+        console.log('logged in')
+      });
 
+      const { data } = await Facebook.api('/me/posts')
+      console.log("d",data)
+      setPosts(data)
+    })()
+  }, [])
   return {
     posts,
   }
